f71ccfc1cb40d483488cae6807869c48dc42331a
raees.aamir <raees.aamir@yandex.com>
2019-09-19 23:34:30 +0100

    Added lorem ipsum content to home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Jumbotron,
+  Button,
+  ListGroup,
+  ListGroupItem,
+} from 'reactstrap';
 
 function Home() {
   return (
     <>
-      Hello world
+      <Container>
+        <Row>
+          <Col>
+            <Jumbotron>
+              <h1 className="display-3">Hello, world!</h1>
+              <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+              <hr className="my-2" />
+              <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+              <p className="lead">
+                <Button color="primary">Learn More</Button>
+              </p>
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ListGroup>
+              <ListGroupItem>Cras justo odio</ListGroupItem>
+              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+              <ListGroupItem>Morbi leo risus</ListGroupItem>
+              <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+              <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
