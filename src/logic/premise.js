c1b7999513709348471bac7c7f0b075ec7b43c0d
raees.aamir <raees.aamir@yandex.com>
2019-09-23 15:11:52 +0100

    Created a method to convert a premise to a set

--- src/logic/premise.js
+++ src/logic/premise.js
@@ -1,3 +1,10 @@
+import {
+  ALL_A_IS_B,
+  SOME_A_IS_B,
+  SOME_A_IS_NOT_B,
+  NO_A_IS_B,
+} from './forms';
+
 class Premise {
   constructor(form, terms) {
     this.form = form;
@@ -17,6 +24,29 @@ class Premise {
 
   toString() {
     return JSON.stringify(this);
+  }
+
+  copyToSet(a = new Set(), b = new Set()) {
+    switch (this.form) {
+      case ALL_A_IS_B:
+        a.add(0);
+        b.add(1);
+        b.add(1);
+        break;
+      case SOME_A_IS_NOT_B:
+      case SOME_A_IS_B:
+        a.add(2);
+        a.add(3);
+        b.add(3);
+        b.add(4);
+        break;
+      case NO_A_IS_B:
+        a.add(5);
+        b.add(6);
+        break;
+      default:
+        break;
+    }
   }
 }
 
