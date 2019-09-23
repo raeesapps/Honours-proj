9a4f3894e00163a29d614bf7f7c5f98bc734812e
raees.aamir <raees.aamir@yandex.com>
2019-09-23 15:12:12 +0100

    Created a method that constructs a set theoretic representation of a premise

--- src/logic/premisePair.js
+++ src/logic/premisePair.js
@@ -1,3 +1,10 @@
+import {
+  M_P_S_M,
+  P_M_S_M,
+  M_P_M_S,
+  P_M_M_S,
+} from './figure';
+
 class PremisePair {
   constructor(premises, relationship) {
     this.premises = premises;
@@ -17,6 +24,38 @@ class PremisePair {
 
   toString() {
     return JSON.stringify(this.premises, this.relationship);
+  }
+
+  toSetTheoreticRepresentation() {
+    const { firstPremise, secondPremise } = this.getPremises();
+    const relationship = this.getRelationship();
+
+    const m = new Set();
+    const p = new Set();
+    const s = new Set();
+
+    switch (relationship) {
+      case M_P_M_S:
+        firstPremise.copyToSet(m, p);
+        secondPremise.copyToSet(m, s);
+        break;
+      case P_M_S_M:
+        firstPremise.copyToSet(p, m);
+        secondPremise.copyToSet(s, m);
+        break;
+      case M_P_S_M:
+        firstPremise.copyToSet(m, p);
+        secondPremise.copyToSet(s, m);
+        break;
+      case P_M_M_S:
+        firstPremise.copyToSet(p, m);
+        secondPremise.copyToSet(m, s);
+        break;
+      default:
+        break;
+    }
+
+    return { m, p, s };
   }
 }
 
