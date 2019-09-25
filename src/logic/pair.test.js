ff359d4b3e3c615b58668f3548ef443daeadaf97
raees.aamir <raees.aamir@yandex.com>
2019-09-25 14:44:56 +0100

    Unit tests for syllogistic premises (barbara)

--- src/logic/pair.test.js
+++ src/logic/pair.test.js
@@ -4,8 +4,12 @@ import {
 import {
   ALL_A_IS_B,
 } from './forms';
+import Logic from './logic';
 import Pair from './pair';
 import Premise from './premise';
+import {
+  threeSetRegions
+} from './regions';
 
 const allMenAreMortalPremise = new Premise(ALL_A_IS_B, { firstTerm: 'Men', secondTerm: 'Mortal' });
 const allGreeksAreMenPremise = new Premise(ALL_A_IS_B, { firstTerm: 'Greeks', secondTerm: 'Mortal' });
@@ -28,4 +32,27 @@ test('Pair has correct relationship', () => {
   const expectedRelationship = M_P_S_M;
   const actualRelationship = pair.relationship;
   expect(expectedRelationship).toBe(actualRelationship);
+});
+
+test('Pair is equivalent to BARBARA', () => {
+  const {
+    M,
+    S,
+    P,
+    M_AND_S,
+    M_AND_P,
+    S_AND_P,
+    M_AND_S_AND_P,
+  } = threeSetRegions;
+  const expectedRegions = JSON.stringify({
+    [M]: Logic.false(),
+    [S]: Logic.false(),
+    [P]: Logic.true(),
+    [M_AND_S]: Logic.false(),
+    [M_AND_P]: Logic.true(),
+    [S_AND_P]: Logic.false(),
+    [M_AND_S_AND_P]: Logic.true(),
+  });
+  const actualRegion = JSON.stringify(pair.mergeSets());
+  expect(actualRegion).toBe(expectedRegions);
 });
