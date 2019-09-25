95d17889075ab2bd027475e955edacda98b667ba
raees.aamir <raees.aamir@yandex.com>
2019-09-25 15:40:09 +0100

    Unit test for CELARENT

--- src/logic/pair.test.js
+++ src/logic/pair.test.js
@@ -1,9 +1,5 @@
-import {
-  M_P_S_M,
-} from './figure';
-import {
-  ALL_A_IS_B,
-} from './forms';
+import figure from './figure';
+import forms from './forms';
 import Logic from './logic';
 import Pair from './pair';
 import Premise from './premise';
@@ -11,48 +7,75 @@ import {
   threeSetRegions
 } from './regions';
 
-const allMenAreMortalPremise = new Premise(ALL_A_IS_B, { firstTerm: 'Men', secondTerm: 'Mortal' });
-const allGreeksAreMenPremise = new Premise(ALL_A_IS_B, { firstTerm: 'Greeks', secondTerm: 'Mortal' });
-const relationship = M_P_S_M;
-const pair = new Pair({
-  firstPremise: allMenAreMortalPremise,
-  secondPremise: allGreeksAreMenPremise,
-}, relationship);
+const {
+  ALL_A_IS_B,
+  NO_A_IS_B,
+} = forms;
 
-test('Pair has correct premises', () => {
-  const { firstPremise, secondPremise } = pair.premises;
-  const expectedFirstPremise = new Premise(ALL_A_IS_B, { firstTerm: 'Men', secondTerm: 'Mortal' });
-  const expectedSecondPremise = new Premise(ALL_A_IS_B, { firstTerm: 'Greeks', secondTerm: 'Mortal' });
+const {
+  M_P_S_M
+} = figure;
 
-  expect(firstPremise.toString()).toEqual(expectedFirstPremise.toString());
-  expect(secondPremise.toString()).toEqual(expectedSecondPremise.toString());
-});
+describe('BARBARA setup', () => {
+  const allMenAreMortalPremise = new Premise(ALL_A_IS_B, { firstTerm: 'Men', secondTerm: 'Mortal' });
+  const allGreeksAreMenPremise = new Premise(ALL_A_IS_B, { firstTerm: 'Greeks', secondTerm: 'Mortal' });
+  const pair = new Pair({
+    firstPremise: allMenAreMortalPremise,
+    secondPremise: allGreeksAreMenPremise,
+  }, M_P_S_M);
 
-test('Pair has correct relationship', () => {
-  const expectedRelationship = M_P_S_M;
-  const actualRelationship = pair.relationship;
-  expect(expectedRelationship).toBe(actualRelationship);
-});
-
-test('Pair is equivalent to BARBARA', () => {
-  const {
-    M,
-    S,
-    P,
-    M_AND_S,
-    M_AND_P,
-    S_AND_P,
-    M_AND_S_AND_P,
-  } = threeSetRegions;
-  const expectedRegions = JSON.stringify({
-    [M]: Logic.false(),
-    [S]: Logic.false(),
-    [P]: Logic.true(),
-    [M_AND_S]: Logic.false(),
-    [M_AND_P]: Logic.true(),
-    [S_AND_P]: Logic.false(),
-    [M_AND_S_AND_P]: Logic.true(),
+  test('Pair is equivalent to BARBARA', () => {
+    const {
+      M,
+      S,
+      P,
+      M_AND_S,
+      M_AND_P,
+      S_AND_P,
+      M_AND_S_AND_P,
+    } = threeSetRegions;
+    const expectedRegions = JSON.stringify({
+      [M]: Logic.false(),
+      [S]: Logic.false(),
+      [P]: Logic.true(),
+      [M_AND_S]: Logic.false(),
+      [M_AND_P]: Logic.true(),
+      [S_AND_P]: Logic.false(),
+      [M_AND_S_AND_P]: Logic.true(),
+    });
+    const actualRegions = JSON.stringify(pair.mergeSets());
+    expect(actualRegions).toBe(expectedRegions);
   });
-  const actualRegion = JSON.stringify(pair.mergeSets());
-  expect(actualRegion).toBe(expectedRegions);
+});
+
+describe('CELARENT SETUP', () => {
+  const noReptilesHaveFurPremise = new Premise(NO_A_IS_B, { firstTerm: 'reptiles', secondTerm: 'greeks' });
+  const allSnakesAreReptilesPremise = new Premise(ALL_A_IS_B, { firstTerm: 'snakes', secondTerm: 'reptiles' });
+  const pair = new Pair({
+    firstPremise: noReptilesHaveFurPremise,
+    secondPremise: allSnakesAreReptilesPremise,
+  }, M_P_S_M);
+
+  test('Pair is equivalent to CELARENT', () => {
+    const {
+      M,
+      S,
+      P,
+      M_AND_S,
+      M_AND_P,
+      S_AND_P,
+      M_AND_S_AND_P,
+    } = threeSetRegions;
+    const expectedRegions = JSON.stringify({
+      [M]: Logic.true(),
+      [S]: Logic.false(),
+      [P]: Logic.true(),
+      [M_AND_S]: Logic.true(),
+      [M_AND_P]: Logic.false(),
+      [S_AND_P]: Logic.false(),
+      [M_AND_S_AND_P]: Logic.false(),
+    });
+    const actualRegions = JSON.stringify(pair.mergeSets());
+    expect(actualRegions).toBe(expectedRegions);
+  });
 });
