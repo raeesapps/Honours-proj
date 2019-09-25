7f1c9053a5c3219eca3f1ed60289421d4b81e68d
raees.aamir <raees.aamir@yandex.com>
2019-09-25 14:44:28 +0100

    Created class to represent a syllogistic premise

--- src/logic/premise.js
+++ src/logic/premise.js
@@ -5,6 +5,12 @@ import {
   NO_A_IS_B,
 } from './forms';
 
+import Logic from './logic';
+
+import {
+  twoSetRegions,
+} from './regions';
+
 class Premise {
   constructor(form, terms) {
     this.form = form;
@@ -12,6 +18,55 @@ class Premise {
 
     this.getForm = this.getForm.bind(this);
     this.getTerms = this.getTerms.bind(this);
+    this.createSets = this.createSets.bind(this);
+
+    this.createSets();
+  }
+
+  createSets() {
+    const {
+      A,
+      B,
+      A_AND_B,
+      A_AND_X,
+      B_AND_X,
+      A_AND_B_AND_X,
+    } = twoSetRegions;
+    switch (this.form) {
+      case ALL_A_IS_B:
+        this.sets = {
+          [A_AND_B]: Logic.true(),
+          [A_AND_B_AND_X]: Logic.true(),
+          [A]: Logic.false(),
+          [A_AND_X]: Logic.false(),
+          [B]: Logic.true(),
+          [B_AND_X]: Logic.true(),
+        };
+        break;
+      case NO_A_IS_B:
+        this.sets = {
+          [A_AND_B]: Logic.false(),
+          [A_AND_B_AND_X]: Logic.false(),
+          [B]: Logic.true(),
+          [B_AND_X]: Logic.true(),
+          [A]: Logic.true(),
+          [A_AND_X]: Logic.true(),
+        };
+        break;
+      case SOME_A_IS_NOT_B:
+      case SOME_A_IS_B:
+        this.sets = {
+          [A_AND_B]: Logic.indeterminate(),
+          [A_AND_B_AND_X]: Logic.indeterminate(),
+          [B]: Logic.true(),
+          [B_AND_X]: Logic.true(),
+          [A]: Logic.true(),
+          [A_AND_X]: Logic.true(),
+        };
+        break;
+      default:
+        break;
+    }
   }
 
   getForm() {
