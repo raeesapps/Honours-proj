5d82ecaa698b9f1794d3b10132164d82759fe781
raees.aamir <raees.aamir@yandex.com>
2019-09-25 14:44:41 +0100

    Created class to represent a pair of premises

--- src/logic/pair.js
+++ src/logic/pair.js
@@ -4,6 +4,11 @@ import {
   M_P_M_S,
   P_M_M_S,
 } from './figure';
+import Logic from './logic'
+import {
+  threeSetRegions,
+  twoSetRegions,
+} from './regions';
 
 class Pair {
   constructor(premises, relationship) {
@@ -12,6 +17,7 @@ class Pair {
 
     this.getRelationship = this.getRelationship.bind(this);
     this.getPremises = this.getPremises.bind(this);
+    this.mergeSets = this.mergeSets.bind(this);
   }
 
   getRelationship() {
@@ -24,6 +30,110 @@ class Pair {
 
   toString() {
     return JSON.stringify(this.premises, this.relationship);
+  }
+
+  mergeSets() {
+    const { firstPremise, secondPremise } = this.premises;
+    const setsOfFirstPremise = firstPremise.sets;
+    const setsOfSecondPremise = secondPremise.sets;
+    const {
+      M,
+      S,
+      P,
+      M_AND_S,
+      M_AND_P,
+      S_AND_P,
+      M_AND_S_AND_P,
+    } = threeSetRegions;
+    const {
+      A,
+      B,
+      A_AND_B,
+      A_AND_X,
+      B_AND_X,
+      A_AND_B_AND_X,
+    } = twoSetRegions;
+    function minimumOf(firstSet, secondSet) {
+      return Math.min(firstSet.getValue(), secondSet.getValue());
+    }
+
+    if (this.relationship === M_P_S_M) {
+      // p1 A: M
+      // p1 B: P
+      // p1 X: S
+      // p2 A: S
+      // p2 B: M
+      // p2 X: P
+      const sets = {
+        [M]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A], setsOfSecondPremise[B])),
+        [S]: setsOfSecondPremise[A],
+        [P]: setsOfFirstPremise[B],
+        [M_AND_S]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A_AND_X], setsOfSecondPremise[A_AND_B])),
+        [M_AND_P]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A_AND_B], setsOfSecondPremise[B_AND_X])),
+        [S_AND_P]: Logic.fromNumber(minimumOf(setsOfFirstPremise[B_AND_X], setsOfSecondPremise[A_AND_X])),
+        [M_AND_S_AND_P]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A_AND_B_AND_X], setsOfSecondPremise[A_AND_B_AND_X])),
+      };
+      return sets;
+    }
+    if (this.relationship === M_P_M_S) {
+      // p1 A: M
+      // p1 B: P
+      // p1 X: S
+      // p2 A: M
+      // p2 B: S
+      // p2 X: P
+      const sets = {
+        [M]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A], setsOfSecondPremise[A])),
+        [S]: setsOfSecondPremise[B],
+        [P]: setsOfFirstPremise[B],
+        [M_AND_S]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A_AND_X], setsOfSecondPremise[A_AND_B])),
+        [M_AND_P]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A_AND_B], setsOfSecondPremise[A_AND_X])),
+        [S_AND_P]: Logic.fromNumber(minimumOf(setsOfFirstPremise[B_AND_X], setsOfSecondPremise[B_AND_X])),
+        [M_AND_S_AND_P]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A_AND_B_AND_X], setsOfSecondPremise[A_AND_B_AND_X])),
+      };
+
+      return sets;
+    }
+    if (this.relationship === P_M_M_S) {
+      // p1 A: P
+      // p1 B: M
+      // p1 X: S
+      // p2 A: M
+      // p2 B: S
+      // p2 X: P
+      const sets = {
+        [M]: Logic.fromNumber(minimumOf(setsOfFirstPremise[B], setsOfSecondPremise[A])),
+        [S]: setsOfSecondPremise[B],
+        [P]: setsOfFirstPremise[A],
+        [M_AND_S]: Logic.fromNumber(minimumOf(setsOfFirstPremise[B_AND_X], setsOfSecondPremise[A_AND_B])),
+        [M_AND_P]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A_AND_B], setsOfSecondPremise[A_AND_X])),
+        [S_AND_P]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A_AND_X], setsOfSecondPremise[B_AND_X])),
+        [M_AND_S_AND_P]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A_AND_B_AND_X], setsOfSecondPremise[A_AND_B_AND_X])),
+      };
+
+      return sets;
+    }
+    if (this.relationship === P_M_S_M) {
+      // p1 A: P
+      // p1 B: M
+      // p1 X: S
+      // p2 A: S
+      // p2 B: M
+      // p2 X: P
+      const sets = {
+        [M]: Logic.fromNumber(minimumOf(setsOfFirstPremise[B], setsOfSecondPremise[B])),
+        [S]: setsOfSecondPremise[A],
+        [P]: setsOfFirstPremise[A],
+        [M_AND_S]: Logic.fromNumber(minimumOf(setsOfFirstPremise[B_AND_X], setsOfSecondPremise[A_AND_B])),
+        [M_AND_P]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A_AND_B], setsOfSecondPremise[B_AND_X])),
+        [S_AND_P]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A_AND_X], setsOfSecondPremise[A_AND_X])),
+        [M_AND_S_AND_P]: Logic.fromNumber(minimumOf(setsOfFirstPremise[A_AND_B_AND_X], setsOfSecondPremise[A_AND_B_AND_X])),
+      };
+
+      return sets;
+    }
+
+    return undefined;
   }
 }
 
