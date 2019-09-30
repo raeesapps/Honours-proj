d5c68232e20c609ad99caaccc5778234f5e8d1d0
raees.aamir <raees.aamir@yandex.com>
2019-09-30 17:19:28 +0100

    Hash map class that supports objects as keys

---
+++ src/logic/dictionary.js
@@ -0,0 +1,43 @@
+class HashDictionary {
+  constructor() {
+    this.dictionary = {};
+    this.keyHashToKeyMappings = {};
+  }
+
+  add(key, value) {
+    const hashCode = key.hashCode();
+    this.dictionary[hashCode] = value;
+    this.keyHashToKeyMappings[hashCode] = key;
+  }
+
+  keyObj(keyHash) {
+    return this.keyHashToKeyMappings[keyHash];
+  }
+
+  get(key) {
+    const hashCode = key.hashCode();
+    return this.dictionary[hashCode];
+  }
+
+  size() {
+    return Object.keys(this.dictionary).length;
+  }
+
+  has(key) {
+    return this.get(key) !== undefined;
+  }
+
+  forEach(callback) {
+    return Object.keys(this.dictionary).forEach(callback);
+  }
+
+  map(callback) {
+    return Object.keys(this.dictionary).map(callback);
+  }
+
+  toString() {
+    return JSON.stringify(this.dictionary, this.keyHashToKeyMappings, null, 2);
+  }
+}
+
+export default HashDictionary;
