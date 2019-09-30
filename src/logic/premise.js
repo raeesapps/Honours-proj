2a3654499941e6193b392148bea8fc0e543625ba
raees.aamir <raees.aamir@yandex.com>
2019-09-30 17:20:14 +0100

    Use of table class

--- src/logic/premise.js
+++ src/logic/premise.js
@@ -1,12 +1,21 @@
+import forms from './forms';
 import hash from 'object-hash';
+
+const tableEntryFunctions = Object.freeze({
+  e: function entryForE() {
+    return 'e';
+  },
+  x: function entryForX(seqIdx) {
+    return `x_${seqIdx.toString()}`;
+  },
+});
 
 class Premise {
   constructor(form, terms) {
     this.form = form;
     this.terms = terms;
 
-    this.getForm = this.getForm.bind(this);
-    this.getTerms = this.getTerms.bind(this);
+    this.hashCode = this.hashCode.bind(this);
   }
 
   getForm() {
@@ -15,6 +24,60 @@ class Premise {
 
   getTerms() {
     return this.terms;
+  }
+
+  populateTable(table) {
+    if (!table.has(this)) {
+      console.log('no entry!');
+      return;
+    }
+
+    const {
+      ALL_A_IS_B,
+      NO_A_IS_B,
+      SOME_A_IS_NOT_B,
+      SOME_A_IS_B,
+    } = forms;
+    const { e, x } = tableEntryFunctions;
+    const { firstTerm, secondTerm } = this.terms;
+    const compartmentDictionary = table.get(this);
+    const i = table.size();
+
+    compartmentDictionary.forEach((keyHash) => {
+      const compartment = compartmentDictionary.keyObj(keyHash);
+      const truths = compartment.getTruths();
+      const truthKeys = Object.keys(truths);
+      let criteria = false;
+
+      switch (this.form) {
+        case ALL_A_IS_B:
+          criteria = truthKeys.filter(() => truths[firstTerm] && !truths[secondTerm]).length > 0;
+          if (criteria) {
+            compartmentDictionary.add(compartment, e());
+          }
+          break;
+        case NO_A_IS_B:
+          criteria = truthKeys.filter(() => truths[firstTerm] && truths[secondTerm]).length > 0;
+          if (criteria) {
+            compartmentDictionary.add(compartment, e());
+          }
+          break;
+        case SOME_A_IS_NOT_B:
+          criteria = truthKeys.filter(() => truths[firstTerm] && truths[secondTerm]).length > 0;
+          if (criteria) {
+            compartmentDictionary.add(compartment, x(i));
+          }
+          break;
+        case SOME_A_IS_B:
+          criteria = truthKeys.filter(() => truths[firstTerm] && !truths[secondTerm]).length > 0;
+          if (criteria) {
+            compartmentDictionary.add(compartment, x(i));
+          }
+          break;
+        default:
+          break;
+      }
+    });
   }
 
   hashCode() {
