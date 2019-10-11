f8fd38b21a0be3c4ff5c7f102f1212a74a29ce5b
raees.aamir <raees.aamir@yandex.com>
2019-10-12 00:56:37 +0100

    Added the pages to the routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,7 @@
 import Home from '../views/Home/Home';
+import Practice from '../views/Practice/Practice';
+import Quiz from '../views/Quiz/Quiz';
+import InstantSolver from '../views/InstantSolver/InstantSolver';
 import Venn from '../views/Venn/Venn';
 
 const routes = [
@@ -6,6 +9,21 @@ const routes = [
     name: 'Home',
     path: '/',
     component: Home,
+  },
+  {
+    name: 'Practice',
+    path: '/practice',
+    component: Practice,
+  },
+  {
+    name: 'Quiz',
+    path: '/quiz',
+    component: Quiz,
+  },
+  {
+    name: 'Instant Solver',
+    path: '/solver',
+    component: InstantSolver,
   },
   {
     name: 'Venn',
