3a9fc2b39ae046507a3703039d134b00733e6ff1
raees.aamir <raees.aamir@yandex.com>
2019-10-12 01:24:06 +0100

    Created footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../assets/css/footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span>Soak Up Syllogisms was engineered by Raees Aamir and created as part of the Honours Project at The University of Edinburgh.</span>
+      </div>
+    </footer>
+
+  );
+}
+
+export default Footer;
