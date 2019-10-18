24a7d065dd07176aee71e1f23b4946fd69aead1f
raees.aamir <raees.aamir@yandex.com>
2019-10-18 18:47:36 +0100

    Removed container

--- src/views/Venn/Venn.jsx
+++ src/views/Venn/Venn.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import {
-  Container,
-  Row,
-  Col,
-} from 'reactstrap';
 
 import VennDiagram from '../../components/VennDiagram/VennDiagram';
 
 function Venn() {
   return (
     <>
-      <Container>
-        <Row>
-          <Col>
-            <VennDiagram />
-          </Col>
-        </Row>
-      </Container>
+      <VennDiagram />
     </>
   );
 }
