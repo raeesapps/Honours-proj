c3528dbc81b87069a4b32f32baa94d32068e5fc7
raees.aamir <raees.aamir@yandex.com>
2019-10-18 17:16:39 +0100

    Added project information and license to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Jumbotron,
+} from 'reactstrap';
 import '../../assets/css/footer.css';
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="container">
-        <span>Soak Up Syllogisms was engineered by Raees Aamir and created as part of the Honours Project at The University of Edinburgh.</span>
-      </div>
-    </footer>
 
+      <Container>
+        <Row>
+          <Col>
+            Soak Up Syllogisms was engineered by Raees Aamir and created as part of the Honours Project at The University of Edinburgh,
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            and is licensed under a GNU General Public License (GNU GPL).
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <img alt="license" src="https://www.gnu.org/graphics/gplv3-with-text-84x42.png" />
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 }
 
