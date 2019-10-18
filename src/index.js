8f7b88907a62b64611ff62dbe7121b6ab4ff21fc
raees.aamir <raees.aamir@yandex.com>
2019-10-18 17:15:51 +0100

    Set global font to B612 Mono

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/font.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
