6b64f487bf6bba1e73d417de00607c2843c8ba2b
raees.aamir <raees.aamir@yandex.com>
2019-10-21 01:24:08 +0100

    Added support for multiple premises

--- src/components/Argument/ArgumentForm.jsx
+++ src/components/Argument/ArgumentForm.jsx
@@ -4,10 +4,13 @@ import {
   Button,
   Form,
   FormGroup,
+  Label,
 } from 'reactstrap';
 
 import Argument from '../../logic/argument';
 import PropositionFormGroup from '../Proposition/PropositionFormGroup';
+
+import plus from '../../assets/img/plus.png';
 
 class ArgumentForm extends React.Component {
   constructor(props) {
@@ -15,43 +18,76 @@ class ArgumentForm extends React.Component {
     this.state = {
       alertVisible: false,
       validationSuccessful: false,
+      propositions: [
+        {
+          name: 'Minor premise',
+          ref: React.createRef(),
+        },
+        {
+          name: 'Major premise',
+          ref: React.createRef(),
+        },
+        {
+          name: 'Conclusion',
+          ref: React.createRef(),
+        },
+      ],
     };
-    this.majorRef = React.createRef();
-    this.minorRef = React.createRef();
-    this.conclusionRef = React.createRef();
     this.validate = this.validate.bind(this);
+    this.addProposition = this.addProposition.bind(this);
+  }
+
+  addProposition() {
+    const {
+      propositions,
+    } = this.state;
+
+    const conclusion = propositions.pop();
+
+    propositions.push({
+      name: 'ayy lmao',
+      ref: React.createRef(),
+    });
+
+    propositions.push(conclusion);
+
+    this.setState({ propositions: [...propositions] });
   }
 
   validate() {
     const {
-      minorRef,
-      majorRef,
-      conclusionRef,
-    } = this;
+      propositions,
+    } = this.state;
 
-    const minorProposition = minorRef.current.getPropositionObj();
-    const majorProposition = majorRef.current.getPropositionObj();
-    const argument = new Argument([minorProposition, majorProposition]);
-    const conclusion = conclusionRef.current.getPropositionObj();
+    const argument = new Argument(propositions
+      .filter((proposition) => proposition.name !== 'Conclusion')
+      .map((proposition) => proposition.ref.current.getPropositionObj()));
+
+    const conclusion = propositions
+      .find((proposition) => proposition.name === 'Conclusion')
+      .ref.current.getPropositionObj();
+
     this.setState({ validationSuccessful: argument.argue(conclusion), alertVisible: true });
   }
 
   render() {
     const {
-      minorRef,
-      majorRef,
-      conclusionRef,
-    } = this;
-    const {
       validationSuccessful,
       alertVisible,
+      propositions,
     } = this.state;
     return (
       <>
         <Form>
-          <PropositionFormGroup ref={minorRef} name="Major premise" />
-          <PropositionFormGroup ref={majorRef} name="Minor premise" />
-          <PropositionFormGroup ref={conclusionRef} name="Conclusion" />
+          {
+            propositions.map((proposition) => {
+              const { name, ref } = proposition;
+              return <PropositionFormGroup ref={ref} name={name} />;
+            })
+          }
+          <FormGroup style={{ display: 'inline-block' }}>
+            <Button onClick={this.addProposition}><img style={{ height: '200px', width: '200px' }} src={plus} alt="plus" /></Button>
+          </FormGroup>
           <FormGroup>
             <Button onClick={this.validate}>Validate</Button>
           </FormGroup>
