386429919f225818054cbd8ddfc68bc957fc6aaa
raees.aamir <raees.aamir@yandex.com>
2019-10-25 01:06:13 +0100

    Atoms to components method

--- src/logic/compartment.js
+++ src/logic/compartment.js
@@ -1,6 +1,12 @@
 import hash from 'object-hash';
+import Table from './table';
 
 class Compartment {
+  static getAllCompartments(atoms) {
+    const table = new Table(atoms);
+    return table.getCompartments();
+  }
+
   constructor(truths) {
     this.truths = truths;
 
