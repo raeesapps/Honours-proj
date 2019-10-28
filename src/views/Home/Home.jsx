a95b8cb33f6c8e8f76eb09bca81f9733ea2ed007
raees.aamir <raees.aamir@yandex.com>
2019-10-28 03:05:20 +0000

    Home using MUI

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,47 +1,52 @@
 import React from 'react';
-import {
-  Container,
-  Row,
-  Col,
-  Jumbotron,
-} from 'reactstrap';
 
-function Home() {
+import { withStyles } from '@material-ui/core/styles';
+
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import styles from '../../assets/views/jss/Home/home';
+
+function Home(props) {
+  const { classes } = props;
   return (
-    <>
-      <Container>
-        <Row>
-          <Col>
-            <Jumbotron>
-              <h1 className="lead">Welcome to Soak Up Syllogisms</h1>
-              <p className="lead">Learn how to become an undefeated grandmaster of logical reasoning. Enjoy!</p>
-            </Jumbotron>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div style={{ marginBottom: '10px' }}>
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={3}
+        direction="row"
+        justify="center"
+        alignItems="flex-start"
+      >
+        <Grid item xs={11}>
+          <Paper className={classes.paper}>
+            <Typography style={{ marginBottom: '10px' }} variant="h3">
+              Welcome to Soak Up Syllogisms
+            </Typography>
+            <Typography varient="4">
+              Learn how to become an undefeated grandmaster of reasoning here.
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={11}>
+          <Paper className={classes.paper}>
+            <Typography varient="body2">
               Syllogisms are logical arguments that arrive at a conclusion based on two or more propositions. Soak Up Syllogisms is a tool to help you get familiar with logical reasoning with syllogisms. A number of exercises are available to help you understand the concepts more clearly, you can see the exercises on the practice section. If you are feeling confident, you can go to the quiz section to test yourself.
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div style={{ marginBottom: '10px' }}>
+            </Typography>
+            <Typography varient="body2">
+              <br />
               If syllogisms are completely new to you then you should read the tutorial before proceeding onto the exercises. In the tutorial you will find explanations on the anatomy of a syllogism and how to represent and manipulate them diagrammatically.
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div style={{ marginBotom: '10px' }}>
+            </Typography>
+            <Typography varient="body2">
+              <br />
               The web app also provides the instant solver where you can check if a syllogistic argument is valid.
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </>
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
-export default Home;
+export default withStyles(styles)(Home);
