b74c152fc83419cb46d7898e829197440100b504
raees.aamir <raees.aamir@yandex.com>
2019-10-28 03:05:33 +0000

    Footer using MUI

---
+++ src/assets/components/jss/Footer/footer.js
@@ -0,0 +1,8 @@
+const styles = () => ({
+  root: {
+    marginTop: 'calc(5% + 60px)',
+    bottom: 0,
+  },
+});
+
+export default styles;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,40 +1,40 @@
 import React from 'react';
-import {
-  Container,
-  Row,
-  Col,
-} from 'reactstrap';
-import '../../assets/css/footer.css';
 
-function Footer() {
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+import styles from '../../assets/components/jss/Footer/footer'
+
+function Footer(props) {
+  const { classes } = props;
   return (
-    <footer className="footer">
-      <Container>
-        <Row>
-          <Col>
+    <div className={classes.root}>
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item xs={9}>
+          <Typography>
             Soak Up Syllogisms was engineered by
             <a href="http://raeesaamir.net"> Raees Aamir </a>
             and created as part of the
             <a href="https://www.inf.ed.ac.uk/teaching/courses/proj/"> Honours Project </a>
             at
             <a href="https://ed.ac.uk"> The University of Edinburgh </a>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
             and is licensed under a GNU General Public License (
             <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPL</a>
             ).
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <img style={{ marginBottom: '10px' }} alt="license" src="https://www.gnu.org/graphics/gplv3-with-text-84x42.png" />
-          </Col>
-        </Row>
-      </Container>
-    </footer>
+          </Typography>
+        </Grid>
+        <Grid item xs={9}>
+          <img style={{ marginBottom: '10px' }} alt="license" src="https://www.gnu.org/graphics/gplv3-with-text-84x42.png" />
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
-export default Footer;
+export default withStyles(styles)(Footer);
