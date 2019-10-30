317ca1351c3f18ca0d16694984637f8e6ad15a30
raees.aamir <raees.aamir@yandex.com>
2019-10-30 11:34:01 +0000

    Trigger snackbar on logic validation

--- src/views/InstantSolver/InstantSolver.jsx
+++ src/views/InstantSolver/InstantSolver.jsx
@@ -6,16 +6,10 @@ import Snackbar from '@material-ui/core/Snackbar';
 import ArgumentForm from '../../components/Argument/ArgumentForm';
 import UninteractiveVennDiagram from '../../components/VennDiagram/UninteractiveVennDiagram';
 import SnackbarWrapper from '../../components/Snackbar/SnackbarWrapper';
-import styles from '../../assets/views/jss/InstantSolver/instant_solver_styles';
 
 import Argument from '../../logic/argument';
-
-const snackbarTypes = Object.freeze({
-  SUCCESS: 'success',
-  ERROR: 'error',
-  WARNING: 'warning',
-  INFO: 'info',
-});
+import snackbarTypes from '../../components/Snackbar/snackbar_types';
+import styles from '../../assets/views/jss/InstantSolver/instant_solver_styles';
 
 class InstantSolver extends React.Component {
   constructor(props) {
@@ -36,15 +30,25 @@ class InstantSolver extends React.Component {
   onSubmitForm() {
     const argumentForm = this.argumentFormRef.current;
     const { premises } = argumentForm.state;
+    const { SUCCESS, ERROR } = snackbarTypes;
     const vennDiagram = this.vennDiagramRef.current;
+
     const argument = new Argument(premises
       .filter((premise) => premise.name !== 'Conclusion')
       .map((premise) => premise.ref.current.getPremiseObj()));
     const conclusion = premises
       .find((premise) => premise.name === 'Conclusion')
       .ref.current.getPremiseObj();
+
     vennDiagram.applyShading(argument);
-    argumentForm.setState({ validationSuccessful: argument.argue(conclusion), alertVisible: true });
+
+    const valid = argument.argue(conclusion);
+
+    if (valid) {
+      this.setState({ snackbarVisible: true, snackbarType: SUCCESS, snackbarMsg: 'Valid!' });
+    } else {
+      this.setState({ snackbarVisible: true, snackbarType: ERROR, snackbarMsg: 'Invalid!' });
+    }
   }
 
   onError(msg) {
