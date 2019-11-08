a6ac7792d9c24d45578453501b22407c220f0cd1
raees.aamir <raees.aamir@yandex.com>
2019-11-08 23:29:25 +0000

    JSS for represent premise question

---
+++ src/assets/views/jss/RepresentPremises/represent_premise_question_styles.js
@@ -0,0 +1,18 @@
+const styles = (theme) => ({
+  root: {
+    width: '90%',
+  },
+  button: {
+    marginTop: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing(2),
+  },
+  resetContainer: {
+    padding: theme.spacing(3),
+  },
+});
+
+export default styles;
+
