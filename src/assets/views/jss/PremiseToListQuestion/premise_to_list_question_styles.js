d552ddecbd1e90467d0676831aa02827cd23e45c
raees.aamir <raees.aamir@yandex.com>
2019-11-26 12:56:35 +0000

    Improved spacing

--- src/assets/views/jss/PremiseToListQuestion/premise_to_list_question_styles.js
+++ src/assets/views/jss/PremiseToListQuestion/premise_to_list_question_styles.js
@@ -1,9 +1,18 @@
-function styles() {
+function styles(theme) {
   return {
-    typography: {
+    haskellTypography: {
       bottom: '25px',
       display: 'inline',
       position: 'relative',
+    },
+    spacing: {
+      marginBottom: theme.spacing.unit * 2,
+    },
+    instructionTypography: {
+      marginBottom: theme.spacing.unit,
+    },
+    titleTypography: {
+      marginBottom: theme.spacing.unit * 3,
     },
   };
 }
