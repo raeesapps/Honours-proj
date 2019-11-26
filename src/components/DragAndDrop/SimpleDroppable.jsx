da8f76f4310d7fdf0e5a4b69ab0e27d677742419
raees.aamir <raees.aamir@yandex.com>
2019-11-26 11:15:05 +0000

    Added alignment parameter

--- src/components/DragAndDrop/SimpleDroppable.jsx
+++ src/components/DragAndDrop/SimpleDroppable.jsx
@@ -4,13 +4,13 @@ import { Droppable } from 'react-beautiful-dnd';
 import SimpleDraggable from './SimpleDraggable';
 import styles from '../../assets/components/jss/DragAndDrop/simple_droppable_styles';
 
-function SimpleDroppable({ items, droppableId }) {
+function SimpleDroppable({ items, droppableId, alignment }) {
   return (
     <Droppable droppableId={droppableId} direction="horizontal">
       {(provided, snapshot) => (
         <div
           ref={provided.innerRef}
-          style={styles(snapshot.isDraggingOver)}
+          style={styles(snapshot.isDraggingOver, alignment)}
           {...provided.droppableProps}
         >
           {items.map((item, index) => (
