c94a813bf1d1f17fcfdbf236389e8255508872b4
raees.aamir <raees.aamir@yandex.com>
2019-11-29 10:55:00 +0000

    Renamed paths to the premise to list question

---
+++ src/assets/views/jss/ArgumentToListQuestion/PremiseToListStep/premise_to_list_step_styles.js
@@ -0,0 +1,20 @@
+function styles(theme) {
+  return {
+    haskellTypography: {
+      bottom: '25px',
+      display: 'inline',
+      position: 'relative',
+    },
+    spacing: {
+      marginBottom: theme.spacing.unit * 2,
+    },
+    instructionTypography: {
+      marginBottom: theme.spacing.unit,
+    },
+    titleTypography: {
+      marginBottom: theme.spacing.unit * 3,
+    },
+  };
+}
+
+export default styles;
