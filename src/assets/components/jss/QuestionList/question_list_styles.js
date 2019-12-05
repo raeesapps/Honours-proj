65d71251b88fce377d0ce3250be6bcd09d1cba9f
raees.aamir <raees.aamir@yandex.com>
2019-12-05 14:28:02 +0000

    Question list component

---
+++ src/assets/components/jss/QuestionList/question_list_styles.js
@@ -0,0 +1,11 @@
+function styles(theme) {
+  return {
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+  };
+}
+
+export default styles;
---
+++ src/components/QuestionList/QuestionList.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import styles from '../../assets/components/jss/QuestionList/question_list_styles';
+
+function QuestionList(props) {
+  const { questions, path, classes } = props;
+
+  return (
+    <List className={classes.root}>
+      {
+        questions.map((question) => (
+          <NavLink to={{
+            pathname: path,
+            question,
+          }}>
+            <ListItem button>
+              <ListItemText>
+                {question.title}
+              </ListItemText>
+            </ListItem>
+          </NavLink>
+        ))
+      }
+    </List>
+  )
+}
+
+export default withStyles(styles)(QuestionList);
