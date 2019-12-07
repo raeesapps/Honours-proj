8028b9a77ee0cf68e088a8025528559d4e60fde8
raees.aamir <raees.aamir@yandex.com>
2019-12-07 13:47:26 +0000

    Messed up this commit

---
+++ src/views/PremiseToDiagramQuestion/PremiseToDiagramStep.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import { stages, validate } from '../../logic/premise_validator';
+
+import InteractiveVennDiagram from '../../components/VennDiagram/InteractiveVennDiagram';
+
+import styles from '../../assets/views/jss/PremiseToDiagramQuestion/premise_to_diagram_step_styles';
+
+class PremiseToDiagramStep extends React.Component {
+  constructor() {
+    super();
+    this.vennDiagramRef = React.createRef();
+
+    this.validate = this.validate.bind(this);
+    this.toSentenceAndVennDiagram = this.toSentenceAndVennDiagram.bind(this);
+  }
+
+  validate() {
+    const { REPRESENTATION_STAGE } = stages;
+    const { vennDiagramRef } = this;
+    const { premise } = this.props;
+
+    return validate([premise], [vennDiagramRef], REPRESENTATION_STAGE);
+  }
+
+  toSentenceAndVennDiagram(premise) {
+    const { vennDiagramRef } = this;
+    const { vennDiagramShading } = this.props;
+
+    const title = premise.toSentence();
+
+    return (
+      <Grid item xs={5}>
+        <Typography variant="subtitle2">
+          {
+            title
+          }
+        </Typography>
+        <InteractiveVennDiagram title={`${title}rep`} premise={premise} ref={vennDiagramRef} shadings={vennDiagramShading} />
+      </Grid>
+    );
+  }
+
+  render() {
+    const { classes, premise } = this.props;
+
+    return (
+      <div>
+        <Grid container>
+          {
+            this.toSentenceAndVennDiagram(premise)
+          }
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(PremiseToDiagramStep);
