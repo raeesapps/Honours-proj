39f04fe706f6e8878ec0ce77cb1519b7dc4109db
raees.aamir <raees.aamir@yandex.com>
2019-12-27 22:00:36 +0000

    Improved home page

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,53 +2,53 @@ import React from 'react';
 
 import { withStyles } from '@material-ui/core/styles';
 
+import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
-import syllogisms from '../../assets/views/img/Home/syllogisms.png';
 import styles from '../../assets/views/jss/Home/home_styles';
 
 function Home(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
-      <Grid
-        container
-        spacing={3}
-        direction="row"
-        justify="center"
-        alignItems="flex-start"
-      >
-        <Grid item xs={11}>
-          <Paper className={classes.paper}>
-            <Typography style={{ marginBottom: '10px' }} variant="h3">
-              Welcome to Soak Up Syllogisms
-            </Typography>
-            <Typography varient="4">
-              Learn how to become an undefeated grandmaster of reasoning here.
-            </Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={11}>
-          <Paper className={classes.paper}>
-            <Typography varient="body2">
-              Syllogisms are logical arguments that arrive at a conclusion based on two or more propositions. Soak Up Syllogisms is a tool to help you get familiar with logical reasoning with syllogisms. A number of exercises are available to help you understand the concepts more clearly, you can see the exercises on the practice section. If you are feeling confident, you can go to the quiz section to test yourself.
-            </Typography>
-            <Typography varient="body2">
-              <br />
-              If syllogisms are completely new to you then you should read the tutorial before proceeding onto the exercises. In the tutorial you will find explanations on the anatomy of a syllogism and how to represent and manipulate them diagrammatically.
-            </Typography>
-            <Typography varient="body2">
-              <br />
-              The web app also provides the instant solver where you can check if a syllogistic argument is valid.
-            </Typography>
-          </Paper>
-          <center>
-            <img src={syllogisms} alt="syllogisms" />
-          </center>
-        </Grid>
-      </Grid>
+      <Container>
+        <Paper className={classes.paper}>
+          <Typography style={{ marginBottom: '10px' }} variant="h3">
+            Welcome to Soak Up Syllogisms
+          </Typography>
+          <Typography variant="subtitle1">
+            A fun game where you learn how to become an undefeated grandmaster of logical reasoning.
+          </Typography>
+        </Paper>
+        <br />
+        <Paper className={classes.paper}>
+          <Typography variant="h5">
+            What are Syllogisms?
+          </Typography>
+          <Typography variant="body2">
+            Syllogisms are logical arguments that arrive at a conclusion based on two or more propositions. Syllogisms with more than two propositions are known as polysyllogisms.
+          </Typography>
+        </Paper>
+        <br />
+        <Paper className={classes.paper}>
+          <Typography variant="h5">
+            How does Soak Up Syllogisms help you learn syllogisms?
+          </Typography>
+          <br />
+          <Typography variant="body2">
+            Soak Up Syllogisms is a tool to help you get familiar with Aristotelian syllogisms and polysyllogisms. Five different types of exercises are available to help you understand the concepts more clearly. You can see the exercises by clicking the burger icon; you can practice them or if you are feeling confident, you can go to the quiz section to test yourself.
+          </Typography>
+          <br />
+          <Typography variant="body2">
+            If syllogisms are completely new to you then you should read the tutorial before proceeding onto the exercises. In the tutorial you will find explanations on the anatomy of a syllogism and how to represent and manipulate them using venn diagrams. Elliptic venn diagrams are required to solve problems involving polysyllogisms; an explanation of elliptic venn diagrams is also provided.
+          </Typography>
+          <br />
+          <Typography variant="body2">
+            The web app also provides the instant solver where you can check your answers to the exercises or an exam question. Please try to solve questions before using the Instant Solver! To understand concepts you must spend time reasoning to derive the correct answer rather than merely using the instant solver.
+          </Typography>
+        </Paper>
+      </Container>
     </div>
   );
 }
