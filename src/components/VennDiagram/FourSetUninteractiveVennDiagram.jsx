46617fadb6abdc0f5ab10a4cda9f8fe56b2abe18
raees.aamir <raees.aamir@yandex.com>
2019-12-28 23:20:52 +0000

    Four Set Venn argument shading implemented

--- src/components/VennDiagram/FourSetUninteractiveVennDiagram.jsx
+++ src/components/VennDiagram/FourSetUninteractiveVennDiagram.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import * as d3 from 'd3';
+
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
 import {
   createFourSetEllipticVennDiagram,
   ellipses,
+  mapRegion,
 } from './venn_utils';
 
 import styles from '../../assets/components/jss/VennDiagram/four_set_uninteractive_venn_diagram_styles';
@@ -47,14 +50,50 @@ class FourSetUninteractiveVennDiagram extends React.Component {
       c: null,
       d: null,
     };
-    this.applyShadings = this.applyShadings.bind(this);
+    this.applyShading = this.applyShading.bind(this);
   }
 
   componentDidMount() {
     this.div = createFourSetEllipticVennDiagram('ellipseVenn', ellipses);
   }
 
-  applyShadings(argument) {
+  applyShading(premiseCollection) {
+    const { BLACK, RED } = shadings;
+    const [a, b, c, d] = premiseCollection.terms;
+
+    if (!(this.state.a && this.state.b && this.state.c && this.state.d)) {
+      this.setState({ a, b, c, d });
+    }
+
+    const mappings = {};
+    this.div.selectAll('path').each(function each() {
+      const node = d3.select(this);
+      const nodeRegion = node.attr('id');
+      const mappedRegion = mapRegion(nodeRegion, a, b, c, d);
+
+      mappings[mappedRegion] = null;
+    });
+    const resolvedColumn = premiseCollection.unifyAndResolve();
+    const premiseCollectionVennDiagramParts = premiseCollection.getVennDiagramParts();
+    premiseCollectionVennDiagramParts.forEach((premiseCollectionVennDiagramPart) => {
+      const { compartment, vennDiagramPart } = premiseCollectionVennDiagramPart;
+      const resolvedValueArray = resolvedColumn[compartment.hashCode()];
+
+      if (resolvedValueArray.length) {
+        const vennDiagramPartSplit = vennDiagramPart.split('\\');
+        const leftPart = vennDiagramPartSplit[0];
+
+        if (!(leftPart in mappings)) {
+          throw new Error(`Shading algorithm failed! ${leftPart} not found in ${JSON.stringify(mappings)}`);
+        }
+
+        const shading = resolvedValueArray[0] === 'e' ? BLACK : RED;
+        mappings[leftPart] = shading;
+      }
+    });
+    Object.keys(mappings).forEach((mapping) => {
+      shadeRegions(this.div, mapping, mappings[mapping]);
+    });
   }
 
   render() {
