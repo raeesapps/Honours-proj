da28632581151328f8b5868e64172adaa31f12ce
raees.aamir <raees.aamir@yandex.com>
2019-12-30 21:37:46 +0000

    Changed mui theme

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -1,14 +1,9 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import teal from '@material-ui/core/colors/teal';
-import blue from '@material-ui/core/colors/blue';
+import lightBlue from '@material-ui/core/colors/lightBlue';
 
 const theme = createMuiTheme({
   palette: {
-    primary: teal,
-    secondary: blue,
-  },
-  status: {
-    danger: 'orange',
+    primary: lightBlue,
   },
 });
 
