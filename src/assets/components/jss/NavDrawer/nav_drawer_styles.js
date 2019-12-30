162f1de78fabbbb8d0c1c94ccdd8151d5baf53d2
raees.aamir <raees.aamir@yandex.com>
2019-12-30 21:38:17 +0000

    Replaced drawer with navbar buttons

--- src/assets/components/jss/NavDrawer/nav_drawer_styles.js
+++ src/assets/components/jss/NavDrawer/nav_drawer_styles.js
@@ -1,60 +1,11 @@
-const DRAWER_WIDTH = 240;
-
 function styles(theme) {
   return {
     root: {
-      display: 'flex',
-      marginBottom: '3%',
-    },
-    appBar: {
-      transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-    },
-    appBarShift: {
-      width: `calc(100% - ${DRAWER_WIDTH}px)`,
-      marginLeft: DRAWER_WIDTH,
-      transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    menuButton: {
-      marginLeft: 12,
-      marginRight: 20,
-    },
-    hide: {
-      display: 'none',
-    },
-    drawer: {
-      width: DRAWER_WIDTH,
-      flexShrink: 0,
-    },
-    image: {
-      maxWidth: '200px',
-      maxheight: '200px',
-      margin: '20px 15px',
-      padding: '5px'
-    },
-    drawerPaper: {
-      width: DRAWER_WIDTH,
-    },
-    content: {
       flexGrow: 1,
-      padding: theme.spacing.unit * 3,
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      marginLeft: -DRAWER_WIDTH,
+      marginBottom: theme.spacing.unit * 12,
     },
-    contentShift: {
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
+    link: {
+      margin: 'auto',
     },
   };
 }
