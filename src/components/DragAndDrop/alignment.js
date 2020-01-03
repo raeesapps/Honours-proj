ec2f7786b3d73811ef80f07a1caebb1f124fbfcb
raees.aamir <raees.aamir@yandex.com>
2020-01-03 21:43:15 +0000

    Removed argument to list question code
    Upon reflection haskell form is too confusing when combined with other syllogism questions

--- src/components/DragAndDrop/alignment.js
+++
@@ -1,6 +0,0 @@
-const alignment = Object.freeze({
-  HORIZONTAL: 1,
-  VERTICAL: 2,
-});
-
-export default alignment;
--- src/components/DragAndDrop/reorder.js
+++
@@ -1,6 +0,0 @@
-export default function reorder(list, startIndex, endIndex) {
-  const result = [...list];
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-  return result;
-}
