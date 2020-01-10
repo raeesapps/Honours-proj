c485c0efae15f69e021300f196b312b1c2a2b215
raees.aamir <raees.aamir@yandex.com>
2020-01-10 21:35:18 +0000

    Uses the higher order question template component

--- src/views/PremiseToDiagramQuestion/PremiseToDiagramQuestion.jsx
+++ src/views/PremiseToDiagramQuestion/PremiseToDiagramQuestion.jsx
@@ -2,32 +2,18 @@ import React from 'react';
 
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 import PremiseToDiagram from '../../components/PremiseToDiagram/PremiseToDiagram';
-import SnackbarWrapper from '../../components/Snackbar/SnackbarWrapper';
-import snackbarTypes from '../../components/Snackbar/snackbar_types';
 import withSidebar from '../../components/Questions/SidebarHOC';
+import withQuestionTemplate from '../../components/Questions/QuestionTemplate';
 import styles from '../../assets/views/jss/PremiseToDiagramQuestion/premise_to_diagram_question_styles';
 
 class PremiseToDiagramView extends React.Component {
-  constructor() {
-    const { ERROR } = snackbarTypes;
-
-    super();
-    this.state = {
-      premiseCollection: null,
-      showSnackbar: false,
-      snackbarType: ERROR,
-    };
-    this.vennDiagramRef = React.createRef();
-    this.validate = this.validate.bind(this);
-  }
-
-  componentWillMount() {
-    const { location } = this.props;
+  constructor(props) {
+    super(props);
+    const { location } = props;
     const { question } = location;
 
     if (!question) {
@@ -35,74 +21,47 @@ class PremiseToDiagramView extends React.Component {
     }
 
     const { content } = question;
-
-    this.setState({
-      premiseCollection: content,
-    });
+    this.premiseCollection = content;
+    this.vennDiagramRef = React.createRef();
+    this.validate = this.validate.bind(this);
   }
 
   validate() {
-    const { ERROR, SUCCESS } = snackbarTypes;
+    const { onValidate } = this.props;
 
     if (!this.vennDiagramRef.current) {
       throw new Error('Ref not set!');
     }
 
     const result = this.vennDiagramRef.current.validate();
-
-    if (result) {
-      this.setState({
-        snackbarType: SUCCESS,
-        showSnackbar: true,
-      });
-    } else {
-      this.setState({
-        snackbarType: ERROR,
-        showSnackbar: true,
-      });
-    }
+    onValidate(result, 'Incorrect!');
   }
 
   render() {
-    const { ERROR } = snackbarTypes;
-
     const { classes } = this.props;
-    const { premiseCollection, snackbarType, showSnackbar } = this.state;
 
-    if (!premiseCollection.premises.length) {
+    if (!this.premiseCollection.premises.length) {
       throw new Error('No premises in premise collection!');
     }
 
-    const premise = premiseCollection.premises[0];
+    const premise = this.premiseCollection.premises[0];
 
-    const snackbarWrapperDisplayVal = !showSnackbar ? 'none' : '';
-    const snackbarMessage = snackbarType === ERROR ? 'Incorrect!' : 'Correct!';
-    const width = premiseCollection.terms.length === 4 ? { width: '70%' } : { width: '35%' };
+    const width = this.premiseCollection.terms.length === 4 ? { width: '70%' } : { width: '35%' };
     return (
       <div>
-        <Container>
-          <SnackbarWrapper
-            style={{ display: snackbarWrapperDisplayVal, marginBottom: '10px' }}
-            variant={snackbarType}
-            message={snackbarMessage}
-            onClose={() => {
-              this.setState({ showSnackbar: false });
-            }}
-          />
-          <Typography className={classes.instructions} variant="h5">
-            Shade the Venn Diagram to represent
-            {
-              ` "${premise.toSymbolicForm()}" `
-            }
-          </Typography>
-          <Paper className={classes.paper} style={width}>
-            <PremiseToDiagram renderTitle={false} ref={this.vennDiagramRef} premiseCollection={premiseCollection} />
-          </Paper>
-          <Button variant="contained" color="primary" onClick={this.validate}>Validate</Button>
-        </Container>
+        <Typography className={classes.instructions} variant="h5">
+          Shade the Venn Diagram to represent
+          {
+            ` "${premise.toSymbolicForm()}" `
+          }
+        </Typography>
+        <Paper className={classes.paper} style={width}>
+          <PremiseToDiagram renderTitle={false} ref={this.vennDiagramRef} premiseCollection={this.premiseCollection} />
+        </Paper>
+        <Button variant="contained" color="primary" onClick={this.validate}>Validate</Button>
       </div>
     );
   }
 }
 
-export default withStyles(styles)(withSidebar(PremiseToDiagramView));
+export default withStyles(styles)(withSidebar(withQuestionTemplate(PremiseToDiagramView)));
