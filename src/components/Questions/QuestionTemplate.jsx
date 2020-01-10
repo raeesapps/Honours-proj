7c18525d5595e2f1553d1b9fb37adccf71864bad
raees.aamir <raees.aamir@yandex.com>
2020-01-10 21:35:02 +0000

    A question template higher order component

---
+++ src/components/Questions/QuestionTemplate.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import Container from '@material-ui/core/Container';
+
+import SnackbarWrapper from '../Snackbar/SnackbarWrapper';
+import snackbarTypes from '../Snackbar/snackbar_types';
+
+const { ERROR, SUCCESS } = snackbarTypes;
+
+function withQuestionTemplate(WrappedComponent) {
+  return class extends React.Component {
+    constructor() {
+      super();
+
+      this.state = {
+        snackbarType: ERROR,
+        showSnackbar: false,
+        incorrectMesssage: null,
+      };
+      this.onValidate = this.onValidate.bind(this);
+    }
+
+    onValidate(result, incorrectMessage) {
+      const snackbarType = result ? SUCCESS : ERROR;
+      this.setState({
+        showSnackbar: true,
+        snackbarType,
+        incorrectMessage,
+      });
+    }
+
+    render() {
+      const { showSnackbar, snackbarType, incorrectMessage } = this.state;
+
+      const snackbarWrapperDisplayVal = !showSnackbar ? 'none' : '';
+      const snackbarMessage = snackbarType === SUCCESS ? 'Correct!' : incorrectMessage;
+
+      return (
+        <Container>
+          <SnackbarWrapper
+            style={{ display: snackbarWrapperDisplayVal, marginBottom: '10px' }}
+            variant={snackbarType}
+            message={snackbarMessage}
+            onClose={() => {
+              this.setState({ showSnackbar: false });
+            }}
+          />
+          <WrappedComponent onValidate={this.onValidate} {...this.props} />
+        </Container>
+      );
+    }
+  };
+}
+
+export default withQuestionTemplate;
