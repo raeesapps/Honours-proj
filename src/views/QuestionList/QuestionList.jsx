986b908fb8787eec7f943e7817fb55de445be86b
raees.aamir <raees.aamir@yandex.com>
2020-01-12 14:41:02 +0000

    Uses higher order component

--- src/views/QuestionList/QuestionList.jsx
+++ src/views/QuestionList/QuestionList.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import { withStyles } from '@material-ui/core/styles';
 
-import Questions from '../../components/Questions/Questions';
+import withSidebar from '../../components/Questions/QuestionSidebar';
 
 import styles from '../../assets/views/jss/QuestionList/question_list_styles';
 
 function QuestionList() {
-  return <Questions />;
+  return <div />;
 }
 
-export default withStyles(styles)(QuestionList);
+export default withStyles(styles)(withSidebar(QuestionList));
