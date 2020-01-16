73941cbfae5f7e5ace6421e55d91108c6939e975
raees.aamir <raees.aamir@yandex.com>
2020-01-16 14:28:20 +0000

    Created a card for question types

---
+++ src/components/Card/SimpleCard.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+function SimpleCard(props) {
+  const {
+    title,
+    content,
+    path,
+    question,
+    ...rest
+  } = props;
+  return (
+    <Card {...rest}>
+      <NavLink
+        style={{ color: 'inherit', textDecoration: 'inherit' }}
+        to={{
+          pathname: path,
+          question,
+        }}
+      >
+        <CardActionArea>
+          <CardContent>
+            <center>
+              <Typography gutterBottom variant="h5">
+                {title}
+              </Typography>
+              <Typography variant="subtitle1" color="textSecondary" component="p">
+                {content}
+              </Typography>
+            </center>
+          </CardContent>
+        </CardActionArea>
+      </NavLink>
+    </Card>
+  );
+}
+
+export default React.memo(SimpleCard);
