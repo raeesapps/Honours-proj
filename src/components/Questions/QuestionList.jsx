2c7d60d1f8e8a2c9b29753ae29327e373d6d0a89
raees.aamir <raees.aamir@yandex.com>
2020-01-16 14:52:18 +0000

    Questions list uses expansion panel

--- src/components/Questions/QuestionList.jsx
+++ src/components/Questions/QuestionList.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { NavLink } from 'react-router-dom'
 
 import { withStyles } from '@material-ui/core/styles';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 import styles from '../../assets/components/jss/Questions/question_list_styles';
@@ -21,31 +24,37 @@ function QuestionList(props) {
 
   return (
     <div className={classes.root} {...rest}>
-      <Paper>
-        <Typography variant="h6">
-          {title}
-        </Typography>
-        <List>
-          {
-            questions.map((question) => (
-              <NavLink
-                style={{ color: 'inherit', textDecoration: 'inherit' }}
-                key={question.title}
-                to={{
-                  pathname: path,
-                  question,
-                }}
-              >
-                <ListItem button>
-                  <ListItemText>
-                    {question.title}
-                  </ListItemText>
-                </ListItem>
-              </NavLink>
-            ))
-          }
-        </List>
-      </Paper>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls={`${title}AriaControls`}
+          id={`${title}ExpasionPanel`}
+        >
+          <Typography>{title}</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <List>
+            {
+              questions.map((question) => (
+                <NavLink
+                  style={{ color: 'inherit', textDecoration: 'inherit' }}
+                  key={question.title}
+                  to={{
+                    pathname: path,
+                    question,
+                  }}
+                >
+                  <ListItem button>
+                    <ListItemText>
+                      {question.title}
+                    </ListItemText>
+                  </ListItem>
+                </NavLink>
+              ))
+            }
+          </List>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
     </div>
   );
 }
