e2b812c0fc59d1403b16e717e6cd10f72a0b3343
raees.aamir <raees.aamir@yandex.com>
2020-01-17 15:52:00 +0000

    Removed questions from routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,6 @@
-import CombineDiagramsQuestion from '../views/CombineDiagramsQuestion/CombineDiagramsQuestion';
 import Home from '../views/Home/Home';
 import Practice from '../views/Practice/Practice';
 import InstantSolver from '../views/InstantSolver/InstantSolver';
-import PremiseToSymbolicFormQuestion from '../views/PremiseToSymbolicFormQuestion/PremiseToSymbolicFormQuestion';
-import SyllogismToSymbolicFormQuestion from '../views/SyllogismToSymbolicFormQuestion/SyllogismToSymbolicFormQuestion';
-import PremiseToDiagramQuestion from '../views/PremiseToDiagramQuestion/PremiseToDiagramQuestion';
-import MapAndArgueQuestion from '../views/MapAndArgueQuestion/MapAndArgueQuestion';
 import Tutorial from '../views/Tutorial/Tutorial';
 
 const routes = [
@@ -26,36 +21,6 @@ const routes = [
     path: '/tutorial',
     component: Tutorial,
     displayInDrawer: true,
-  },
-  {
-    name: 'premiseToSymbolicFormQuestion',
-    path: '/premiseToSymbolicFormQuestion',
-    component: PremiseToSymbolicFormQuestion,
-    displayInDrawer: false,
-  },
-  {
-    name: 'syllogismToSymbolicFormQuestion',
-    path: '/syllogismToSymbolicFormQuestion',
-    component: SyllogismToSymbolicFormQuestion,
-    displayInDrawer: false,
-  },
-  {
-    name: 'premiseToDiagramQuestion',
-    path: '/premiseToDiagramQuestion',
-    component: PremiseToDiagramQuestion,
-    displayInDrawer: false,
-  },
-  {
-    name: 'combineDiagramsQuestion',
-    path: '/combineDiagramsQuestion',
-    component: CombineDiagramsQuestion,
-    displayInDrawer: false,
-  },
-  {
-    name: 'mapAndArgueQuestion',
-    path: '/mapAndArgueQuestion',
-    component: MapAndArgueQuestion,
-    displayInDrawer: false,
   },
   {
     name: 'Instant Solver',
