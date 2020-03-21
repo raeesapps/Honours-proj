e1df8dc6c0fcd366e2c3613d8764d9a692d5c9cc
raees.aamir <raees.aamir@yandex.com>
2020-03-21 09:58:42 +0000

    Updated homepage

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -14,44 +14,40 @@ function Home(props) {
     <div className={classes.root}>
       <Container>
         <Paper className={classes.paper}>
-          <Typography style={{ marginBottom: '10px' }} variant="h3">
+          <Typography style={{ marginBottom: '10px' }} variant="h2">
             Welcome to Soak Up Syllogisms
           </Typography>
-          <Typography variant="subtitle1">
-            A fun game where you learn how to become an undefeated grandmaster of logical reasoning.
+          <Typography variant="h5">
+            An educational tool that helps you wrap your mind around representing, manipulating and arguing about syllogisms.
           </Typography>
         </Paper>
         <br />
         <Paper className={classes.paper}>
-          <Typography variant="h5">
+          <Typography variant="h3">
             What are Syllogisms?
           </Typography>
-          <Typography variant="body2">
+          <Typography variant="h5">
             Syllogisms are logical arguments that arrive at a conclusion based on two or more propositions.
-            Syllogisms with more than two propositions are known as polysyllogisms.
+            <br />
+            "All People are Mortal"; "All Greeks are People" => "All Greeks are Mortal" is an example of a syllogism.
           </Typography>
         </Paper>
         <br />
         <Paper className={classes.paper}>
-          <Typography variant="h5">
+          <Typography variant="h3">
             How does Soak Up Syllogisms help you learn syllogisms?
           </Typography>
           <br />
-          <Typography variant="body2">
-            Soak Up Syllogisms is a tool to help you get familiar with Aristotelian syllogisms
-            and polysyllogisms. Five different types of exercises are available to help you understand
-            the concepts more clearly. You can see the exercises by clicking 'Questions' in the Navbar;
-            you can practice them or if you are feeling confident, you can go to the quiz section to test yourself.
+          <Typography variant="h5">
+            Five different types of exercises are available to help you understand
+            the concepts more clearly. You can see the exercises by clicking 'Practice' in the header.
           </Typography>
           <br />
-          <Typography variant="body2">
-            If syllogisms are completely new to you then you should read the tutorial before proceeding
-            onto the exercises. In the tutorial you will find explanations on the anatomy of a syllogism
-            and how to represent and manipulate them using venn diagrams. Elliptic venn diagrams are required
-            to solve problems involving polysyllogisms; an explanation of elliptic venn diagrams is also provided.
+          <Typography variant="h5">
+            If it is your first time using Soak up Syllogisms then you should visit the tutorial. In the tutorial you will be presented with interactive exercises to help you understand how to use the tool.
           </Typography>
           <br />
-          <Typography variant="body2">
+          <Typography variant="h5">
             The web app also provides the instant solver where you can check your answers to the exercises
             or an exam question. Please try to solve questions before using the Instant Solver!
             o understand concepts you must spend time reasoning to derive the correct answer rather than
