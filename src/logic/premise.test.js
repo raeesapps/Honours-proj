88c89409f125e3a609e13ec4d977d17a541e8f41
raees.aamir <raees.aamir@yandex.com>
2020-04-10 13:59:10 +0100

    More tests for premise.js

--- src/logic/premise.test.js
+++ src/logic/premise.test.js
@@ -22,7 +22,7 @@ test('First term of premise is `a` and second term of premise is `b`', () => {
   expect(secondTerm).toBe(expectedSecondTerm);
 });
 
-describe('Fill table tests', () => {
+describe('Proposition characteristic tests', () => {
   const terms = ['a', 'b'];
   let table;
 
@@ -30,95 +30,158 @@ describe('Fill table tests', () => {
     table = new Table(terms);
   });
 
-  test('Adding an `ALL_A_IS_B` results in table column being filled in correctly', () => {
+  describe('A code tests', () => {
     const {
       ALL_A_IS_B,
     } = forms;
     const premise = new Premise(ALL_A_IS_B, { firstTerm: 'a', secondTerm: 'b' });
 
-    table.addPremise(premise);
+    test('Adding an A code proposition results in table column being filled in correctly', () => {
+      table.addPremise(premise);
 
-    const compartments = table.getCompartments();
-    const compartmentDictionary = table.getTableDictionary().get(premise);
+      const compartments = table.getCompartments();
+      const compartmentDictionary = table.getTableDictionary().get(premise);
 
-    compartments.forEach((compartment) => {
-      const truths = compartment.getTruths();
-      const entry = compartmentDictionary.get(compartment);
+      compartments.forEach((compartment) => {
+        const truths = compartment.getTruths();
+        const entry = compartmentDictionary.get(compartment);
 
-      if (truths.a && !truths.b) {
-        expect(entry).toBe('e');
-      } else {
-        expect(entry).toBe(null);
-      }
+        if (truths.a && !truths.b) {
+          expect(entry).toBe('e');
+        } else {
+          expect(entry).toBe(null);
+        }
+      });
+    });
+
+    test('A code proposition sentence is correct', () => {
+      const expectedSentence = 'All a are b';
+      const actualSentence = premise.toSentence();
+      expect(actualSentence).toBe(expectedSentence);
+    });
+
+    test('A code proposition standard form is correct', () => {
+      const expectedStandardForm = 'a ⊨ b';
+      const actualStandardForm = premise.toSymbolicForm();
+      expect(actualStandardForm).toBe(expectedStandardForm);
+    });
+
+    test('A code proposition standard form is correct with different subject and predicate', () => {
+      const expectedStandardFormWithDifferentSubjectAndPredicate = 'c ⊨ d';
+      const actualStandardFormWithDifferentSubjectAndPredicate = premise.toSymbolicForm('c', 'd');
+      expect(actualStandardFormWithDifferentSubjectAndPredicate).toBe(expectedStandardFormWithDifferentSubjectAndPredicate);
     });
   });
 
-  test('Adding a `NO_A_IS_B` results in table column being filled in correctly', () => {
+  //Adding an E code proposition results in table column being filled in correctly
+  describe('E code tests', () => {
     const {
       NO_A_IS_B,
     } = forms;
     const premise = new Premise(NO_A_IS_B, { firstTerm: 'a', secondTerm: 'b' });
 
-    table.addPremise(premise);
+    test('Adding an E code proposition results in table column being filled in correctly', () => {
+      table.addPremise(premise);
 
-    const compartments = table.getCompartments();
-    const compartmentDictionary = table.getTableDictionary().get(premise);
+      const compartments = table.getCompartments();
+      const compartmentDictionary = table.getTableDictionary().get(premise);
 
-    compartments.forEach((compartment) => {
-      const truths = compartment.getTruths();
-      const entry = compartmentDictionary.get(compartment);
+      compartments.forEach((compartment) => {
+        const truths = compartment.getTruths();
+        const entry = compartmentDictionary.get(compartment);
 
-      if (truths.a && truths.b) {
-        expect(entry).toBe('e');
-      } else {
-        expect(entry).toBe(null);
-      }
+        if (truths.a && truths.b) {
+          expect(entry).toBe('e');
+        } else {
+          expect(entry).toBe(null);
+        }
+      });
+    });
+
+    test('E code proposition standard form is correct', () => {
+      const expectedSentence = 'No a are b';
+      const actualSentence = premise.toSentence();
+      expect(actualSentence).toBe(expectedSentence);
+    });
+
+    test('E code proposition standard form is correct', () => {
+      const expectedStandardForm = 'a ⊨ ¬b';
+      const actualStandardForm = premise.toSymbolicForm();
+      expect(actualStandardForm).toBe(expectedStandardForm);
     });
   });
 
-  test('Adding an `SOME_A_IS_B` results in table column being filled in correctly', () => {
+  describe('I code tests', () => {
     const {
       SOME_A_IS_B,
     } = forms;
     const premise = new Premise(SOME_A_IS_B, { firstTerm: 'a', secondTerm: 'b' });
 
-    table.addPremise(premise);
+    test('Adding an I code proposition results in table column being filled in correctly', () => {
+      table.addPremise(premise);
 
-    const compartments = table.getCompartments();
-    const compartmentDictionary = table.getTableDictionary().get(premise);
+      const compartments = table.getCompartments();
+      const compartmentDictionary = table.getTableDictionary().get(premise);
 
-    compartments.forEach((compartment) => {
-      const truths = compartment.getTruths();
-      const entry = compartmentDictionary.get(compartment);
+      compartments.forEach((compartment) => {
+        const truths = compartment.getTruths();
+        const entry = compartmentDictionary.get(compartment);
 
-      if (truths.a && truths.b) {
-        expect(entry).toBe('x_1');
-      } else {
-        expect(entry).toBe(null);
-      }
+        if (truths.a && truths.b) {
+          expect(entry).toBe('x_1');
+        } else {
+          expect(entry).toBe(null);
+        }
+      });
+    });
+
+    test('I code proposition standard form is correct', () => {
+      const expectedSentence = 'Some a are b';
+      const actualSentence = premise.toSentence();
+      expect(actualSentence).toBe(expectedSentence);
+    });
+
+    test('I code proposition standard form is correct', () => {
+      const expectedStandardForm = 'a ⊯ ¬b';
+      const actualStandardForm = premise.toSymbolicForm();
+      expect(actualStandardForm).toBe(expectedStandardForm);
     });
   });
 
-  test('Adding a `SOME_A_IS_NOT_B` results in table column being filled in correctly', () => {
+  describe('O code tests', () => {
     const {
       SOME_A_IS_NOT_B,
     } = forms;
     const premise = new Premise(SOME_A_IS_NOT_B, { firstTerm: 'a', secondTerm: 'b' });
 
-    table.addPremise(premise);
+    test('Adding an O code proposition results in the table column being filled in correctly', () => {
+      table.addPremise(premise);
 
-    const compartments = table.getCompartments();
-    const compartmentDictionary = table.getTableDictionary().get(premise);
+      const compartments = table.getCompartments();
+      const compartmentDictionary = table.getTableDictionary().get(premise);
 
-    compartments.forEach((compartment) => {
-      const truths = compartment.getTruths();
-      const entry = compartmentDictionary.get(compartment);
+      compartments.forEach((compartment) => {
+        const truths = compartment.getTruths();
+        const entry = compartmentDictionary.get(compartment);
 
-      if (truths.a && !truths.b) {
-        expect(entry).toBe('x_1');
-      } else {
-        expect(entry).toBe(null);
-      }
+        if (truths.a && !truths.b) {
+          expect(entry).toBe('x_1');
+        } else {
+          expect(entry).toBe(null);
+        }
+      });
+    });
+
+    test('O code proposition standard form is correct', () => {
+      const expectedSentence = 'Some a are not b';
+      const actualSentence = premise.toSentence();
+      expect(actualSentence).toBe(expectedSentence);
+    });
+
+    test('O code proposition standard form is correct', () => {
+      const expectedStandardForm = 'a ⊯ b';
+      const actualStandardForm = premise.toSymbolicForm();
+      expect(actualStandardForm).toBe(expectedStandardForm);
     });
   });
 
